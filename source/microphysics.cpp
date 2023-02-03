c7ae4fd5d11aa43fc6ad2411141919e000c6ed14
Michael Diaz <michaeldiaz@Michaels-MacBook-Pro-3.local>
2023-02-03 01:04:58 -0800

    Remove debugging code and several commented out subroutines for precipitation fall speed

--- source/microphysics.cpp
+++ source/microphysics.cpp
@@ -186,21 +186,6 @@ void hydrometeor_fallout(double *var,double *vel,int il,int ih,int jl,int jh,dou
 			//-----------------------------------------------------------
 			precip[INDEX2D(i,j)] += piecewise_interp(ZA_edges,&ZW(0),QA_mass,QD_mass,NZ,i,j);
 			
-			
-			#if 0
-			double asum = 0;
-			double dsum = rainfall;
-			
-			for(int k=1;k<NZ;k++){
-				
-				asum += QA_mass[k]*(ZA_edges[k+1]-ZA_edges[k]);
-				dsum += QD_mass[k]*(ZW(k+1)-ZW(k));
-			}
-			if(fabs(asum-dsum)>0.0001){
-				
-				printf("Rain mass compare: %d %d %f %f %f %f\n",big_i[i],big_j[j],asum,dsum,rainfall,dsum+rainfall);
-			}
-			#endif
 			//-----------------------------------------------------------
 			// Divide by density for final mass
 			//-----------------------------------------------------------
@@ -210,7 +195,6 @@ void hydrometeor_fallout(double *var,double *vel,int il,int ih,int jl,int jh,dou
 	}}
 	
 }
-
 
 /*********************************************************************
 * Integrate linear function
@@ -223,9 +207,11 @@ double integrate_linear(double *zin,double *qin,int p,double zl,double zh,bool d
 	double mid_slope;
 	
 	if( (edge_slope0<0 && edge_slope1>0) || (edge_slope0>0 && edge_slope1<0) ){	
+
 		mid_slope = 0;
-		//printf("zero slope\n");
+
 	} else{
+
 		mid_slope = 0.5*(edge_slope0 + edge_slope1);
 	}
 
@@ -236,21 +222,14 @@ double integrate_linear(double *zin,double *qin,int p,double zl,double zh,bool d
 		
 		qp_edge = qin[p];
 		qn_edge = qin[p];
-		//printf("zero edge\n");
 	}
 
 	double t0 = (zl-zin[p]) / (zin[p+1]-zin[p]);
 	double t1 = (zh-zin[p]) / (zin[p+1]-zin[p]);
 	
-	//printf("integrate over %f to %f\n",t0,t1);
-
-	//if(debug){
-		//printf("midpoint = %f %f %f\n",1000*((qp_edge - qn_edge) * 0.5 + qn_edge),1000*qp_edge,1000*qn_edge);
-	//}
-
 	return (qp_edge - qn_edge) * 0.5*t1*t1 + qn_edge * t1 - ((qp_edge - qn_edge) * 0.5*t0*t0 + qn_edge * t0);
 }
-#if 1
+
 /*********************************************************************
 * Piecewise linear interpolation which preserves total mass
 *
@@ -266,20 +245,9 @@ double piecewise_interp(double *zin,double *zout,double *qin,double *qout,int zl
 
 	double factor;
 
-	int test_i = -199;
-	int test_j = -59;
-
-
 	double rainfall = 0;
 
 	for(int i=0;i<zlevs;i++){ qout[i] = 0;}
-
-	//if(big_i[ic]==test_i && big_j[jc]==test_j){
-		//int i = 0;
-		//int j = 0;
-		
-		//printf("e %d %d %d %d %f %f %f %f %f\n",big_i[ic],big_j[jc],i,j,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000);
-		//}
 
 	int highest_negative_level = 1;
 
@@ -290,10 +258,6 @@ double piecewise_interp(double *zin,double *zout,double *qin,double *qout,int zl
 		if(zin[j] < 0 && zin[j+1] < 0){
 			
 			rainfall += qin[j] * (zin[j+1]-zin[j]);
-			
-			if(big_i[ic]==test_i && big_j[jc]==test_j){
-					printf("e %d %d %d %d %f %f %f %f %f\n",big_i[ic],big_j[jc],i,j,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000);
-				}
 			
 			highest_negative_level = j+1;
 			
@@ -308,13 +272,6 @@ double piecewise_interp(double *zin,double *zout,double *qin,double *qout,int zl
 				;
 	}
 	
-	if(big_i[ic]==test_i && big_j[jc]==test_j){
-			printf("rainfall = %f %f\n",rainfall*1000,-zin[highest_negative_level]);
-		}
-	
-		if(big_i[ic]==test_i && big_j[jc]==test_j){
-				printf("lowest level = %d\n",highest_negative_level);
-			}
 	//-----------------------------------------------------------
 	// Compare input and output cells to look for matches.
 	// Watch out for j = 0, the integrate_linear function 
@@ -326,51 +283,35 @@ double piecewise_interp(double *zin,double *zout,double *qin,double *qout,int zl
 		// Overlap (mA4 -> m2)
 		//-----------------------------------------------------------
 		if(zout[i] < zin[j] && zout[i+1] < zin[j+1] && zout[i+1] > zin[j]){
-			if(big_i[ic]==test_i && big_j[jc]==test_j){
-						printf("e %d %d %d %d %f %f %f %f %f\n",big_i[ic],big_j[jc],i,j,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000);
-				}
+
 			qout[i] += integrate_linear(zin,qin,j,zin[j],zout[i+1],false) * ((zin[j+1] - zin[j]) / (zout[i+1]-zout[i]));
 		}
 		//-----------------------------------------------------------
 		// Overlap (mA4 -> m3)
 		//-----------------------------------------------------------
 		if(zout[i] < zin[j+1] && zout[i+1] > zin[j+1] && zout[i] > zin[j]){
-			if(big_i[ic]==test_i && big_j[jc]==test_j){
-						printf("c %d %d %d %d %f %f %f %f %f\n",big_i[ic],big_j[jc],i,j,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000);
-							}
+
 			qout[i] += integrate_linear(zin,qin,j,zout[i],zin[j+1],false) * ((zin[j+1] - zin[j]) / (zout[i+1]-zout[i]));
 		}
 		//-----------------------------------------------------------
 		// Output cell falls completely within input cell (mA5 -> m4)
 		//-----------------------------------------------------------
 		if(zout[i] >= zin[j] && zout[i+1] <= zin[j+1]){
-			if(big_i[ic]==test_i && big_j[jc]==test_j){
-					printf("a %d %d %d %d %f %f %f %f %f\n",big_i[ic],big_j[jc],i,j,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000);
-				}
+
 			qout[i] += integrate_linear(zin,qin,j,zout[i],zout[i+1],false) * ((zin[j+1] - zin[j]) / (zout[i+1]-zout[i]));
 		}
 		//-----------------------------------------------------------
 		// Input cell falls completely within output cell
 		//-----------------------------------------------------------		
 		if(zout[i] < zin[j] && zout[i+1] > zin[j+1]){
-			if(big_i[ic]==test_i && big_j[jc]==test_j){
-					printf("d %d %d %d %d %f %f %f %f %f\n",big_i[ic],big_j[jc],i,j,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000);
-				}
+
 			qout[i] += integrate_linear(zin,qin,j,zin[j],zin[j+1],false) * ((zin[j+1] - zin[j]) / (zout[i+1]-zout[i]));
 		}	
 	}}
-	if(big_i[ic]==test_i && big_j[jc]==test_j){
-			printf("output rainfall %f %d %d\n",rainfall,big_i[ic],big_j[jc]);
-		}
 
-	if(rainfall < 0){
-		//printf("output rainfall %f %d %d %f %f\n",rainfall,big_i[ic],big_j[jc],outLons[big_i[ic]],outLats[big_j[jc]]);
-	}
 
 	return rainfall;
-
 }
-#endif
 
 /*********************************************************************
 * Piecewise constant interpolation which preserves total mass
@@ -418,236 +359,6 @@ double piecewise_constant_interp(double *zin,double *zout,double *qin,double *qo
 	return rainfall;
 }
 
-#if 0
-/*********************************************************************
-* Piecewise constant interpolation which preserves total mass
-*
-* zin - input height levels
-* zout - output height levels
-* qin - input field
-* qout - output field
-* zlevs number of levels (i.e. array length for all input arrays)
-**********************************************************************/
-double piecewise_interp(double *zin,double *zout,double *qin,double *qout,int zlevs,int ic,int jc){
-
-	double factor;
-
-	int test_i = 13;
-	int test_j = 24;
-
-	double test[43];
-	double qin_sum[43];
-	double qin_sum_a[43];
-	double qin_sum_b[43];
-	double qin_sum_c[43];
-	double zt0[43];
-	double zb0[43];
-	double zt1[43];
-	double zb1[43];
-
-	double rainfall = 0;
-
-	for(int i=0;i<zlevs;i++){ qout[i] = 0; test[i] = 0; qin_sum[i] = 0; qin_sum_a[i] = 0; qin_sum_b[i] = 0; qin_sum_c[i] = 0;
-		zt0[i] = 0;zb0[i] = 0;zt1[i] = 0;zb1[i] = 0;}
-
-	for(int i=1;i<zlevs-2;i++){
-	for(int j=1;j<zlevs-2;j++){
-		
-
-		if(zout[i] >= zin[j] && zout[i+1] <= zin[j+1]){
-			
-			factor = 1.0;//(zout[i+1] - zin[j]) / (zout[i+1]-zout[i]);
-			
-			test[i] += factor;
-			
-			qout[i] += integrate_linear(zin,qin,j,zout[i],zout[i+1],false) * ((zin[j+1] - zin[j]) / (zout[i+1]-zout[i]));
-			qin_sum[j] += integrate_linear(zin,qin,j,zout[i],zout[i+1],false);
-			qin_sum_a[j] = integrate_linear(zin,qin,j,zout[i],zout[i+1],false);
-			if(ic==test_i && jc==test_j){
-				//printf("a %f %f %f %f\n",zout[i],1000*qin[j],factor,1000*integrate_linear(zin,qin,j,zout[i],zout[i+1],false));
-			}
-			//qout[i] += factor*qin[j];
-		}
-
-
-		if(zout[i] < zin[j] && zout[i+1] < zin[j+1] && zout[i+1] > zin[j]){
-			
-			factor = (zout[i+1] - zin[j]) / (zout[i+1]-zout[i]);
-			
-			test[i] += factor;
-			qin_sum[j] += integrate_linear(zin,qin,j,zin[j],zout[i+1],false);
-			qin_sum_b[j] = integrate_linear(zin,qin,j,zin[j],zout[i+1],false);
-			zb0[j] = zin[j];
-			zt0[j] = zout[i+1];
-			//qout[i] += factor*qin[j];
-			if(ic==test_i && jc==test_j){
-				//printf("b %f %f %f %f\n",zout[i],1000*qin[j],factor,1000*integrate_linear(zin,qin,j,zin[j],zout[i+1],false));
-				
-				
-			}
-			qout[i] += integrate_linear(zin,qin,j,zin[j],zout[i+1],false) * ((zin[j+1] - zin[j]) / (zout[i+1]-zout[i]));
-		}
-		
-		if(zout[i] < zin[j+1] && zout[i+1] > zin[j+1] && zout[i] > zin[j]){
-			
-			factor = (zin[j+1] - zout[i]) / (zout[i+1]-zout[i]);
-			
-			test[i] += factor;
-			qin_sum[j] += integrate_linear(zin,qin,j,zout[i],zin[j+1],false);
-			qin_sum_c[j] = integrate_linear(zin,qin,j,zout[i],zin[j+1],false);
-			zb1[j] = zout[i];
-			zt1[j] = zin[j+1];
-			//qout[i] += factor*qin[j];
-			if(ic==test_i && jc==test_j){
-				//printf("c %f %f %f %f\n",zout[i],1000*qin[j],factor,1000*integrate_linear(zin,qin,j,zout[i],zin[j+1],false));
-			}
-			qout[i] += integrate_linear(zin,qin,j,zout[i],zin[j+1],false) * ((zin[j+1] - zin[j]) / (zout[i+1]-zout[i]));
-		}
-	}}
-
-	//if(ic==test_i && jc==test_j){
-		
-		//for(int i=0;i<zlevs;i++){
-			
-			//if(zin[i] > 0 && fabs(qin[i]-qin_sum[i]) > 0.0000001){
-			//if(qin[10]*1000>1.0)
-				//printf("%d %d %d %f %f| %f %f %f\n",i,ic,jc,qin[i]*1000,qin_sum[i]*1000,qin_sum_a[i]*1000,qin_sum_b[i]*1000,qin_sum_c[i]*1000);
-				//}
-		
-			//}
-		//}
-
-		
-	if(false && ic==test_i && jc==test_j){
-		for(int i=0;i<zlevs;i++){ printf("%d %f %f %f\n",i,test[i],ZW(i),qout[i]*1000);}
-		for(int i=0;i<zlevs;i++){ printf("%d %f %f %f %f %f %f %f %f %f\n",i,
-		qin_sum[i]*1000,qin_sum_b[i]*1000,qin_sum_c[i]*1000,qin[i]*1000,
-		zb0[i],zt0[i],zb1[i],zt1[i],1000*integrate_linear(zin,qin,i,zin[i],zin[i+1],false));}
-		
-		int i = 17;
-		printf("integration test %f %f\n",
-		1000*integrate_linear(zin,qin,i,zin[i],0.5*(zin[i+1]+zin[i]),false)+
-		1000*integrate_linear(zin,qin,i,0.5*(zin[i+1]+zin[i]),zin[i+1],false),
-		1000*integrate_linear(zin,qin,i,zin[i],zin[i+1],false)
-		);
-	}
-
-	return rainfall;
-
-}
-
-/*********************************************************************
-* Piecewise constant interpolation which preserves total mass
-*
-* zin - input height levels
-* zout - output height levels
-* qin - input field
-* qout - output field
-* zlevs number of levels (i.e. array length for all input arrays)
-**********************************************************************/
-double piecewise_interp2(double *zin,double *zout,double *qin,double *qout,int zlevs,int ic,int jc){
-
-	double factor;
-
-	int test_i = 13;
-	int test_j = 24;
-
-	double test[43];
-
-	double rainfall = 0;
-
-	for(int i=0;i<zlevs;i++){ qout[i] = 0; test[i] = 0;}
-
-	for(int i=1;i<zlevs-1;i++){
-	for(int j=1;j<zlevs-1;j++){
-		
-		//if(zin[i] < zout[0]){
-			
-			//factor = (zout[i+1] - zout[i]) / (zin[j+1]-zin[j]);
-			
-			//rainfall += factor*qin[j];
-			//}
-		
-		if(zout[i] > zin[j]){
-			
-			if(zout[i+1] <= zin[j+1] ){ // mA3->m1
-				
-				factor = (zout[i+1] - zout[i]) / (zout[i+1]-zout[i]);
-				
-				//qout[i] += integrate_linear(zin,qin,j,zout[i],zout[i+1]);
-				qout[i] += factor*qin[j];
-				test[i] += factor;
-				if(ic==test_i && jc==test_j){ printf("a %d %f %f %f %f %f %f\n",i,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000,factor);}
-			}
-			
-			if(zout[i+1] == zin[j+1] ){
-			
-				factor = (zout[i] - zin[j]) / (zout[i+1]-zout[i]);
-				test[i] += factor;
-				//qout[i-1] += factor * qin[j];
-				if(ic==test_i && jc==test_j){ printf("b %d %f %f %f %f %f %f\n",i,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000,factor);}
-			}
-		}
-		
-		if(zout[i] > zin[j] && zout[i] < zin[j+1]){
-			
-			 if(zout[i-1] < zin[j] ){ // mA4-> m2 (lower portion of mA4)
-			
-				factor = ( zout[i] - zin[j]) / (zout[i+1]-zout[i]);
-				test[i-1] += factor;
-				//qout[i-1] += integrate_linear(zin,qin,j,zin[j],zout[i]);
-				qout[i-1] += factor * qin[j];
-				
-				if(ic==test_i && jc==test_j){ printf("c %d %f %f %f %f %f %f\n",i-1,zin[j],zin[j+1],zout[i-1],zout[i],qin[j]*1000,factor);}
-			}
-		
-			 if(zout[i+1] > zin[j+1] ){ // mA4 -> m3 (upper portion of mA4)
-				
-				factor = (zin[j+1] - zout[i]) / (zout[i+1]-zout[i]);
-				test[i] += factor;
-				//qout[i] += integrate_linear(zin,qin,j,zout[i],zin[j+1]);//factor * qin[j];
-				qout[i] += factor * qin[j];
-				
-				if(ic==test_i && jc==test_j){ printf("d %d %f %f %f %f %f %f\n",i,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000,factor);}
-			} 
-		}
-#if 0
-		if(zout[i] < zin[j] && zout[i+1] > zin[j+1]){
-			
-			factor = 1.0;
-			test[j] += factor;
-			qout[i] += factor * qin[j];
-			
-			if(ic==test_i && jc==test_j){ printf("g %d %f %f %f %f %f %f\n",j,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000,factor);}
-		}
-#endif			
-		if(zout[i] == zin[j]){
-		
-			 if(zout[i+1] > zin[j+1] ){
-			
-				factor = (zin[j+1] - zout[i]) / (zout[i+1]-zout[i]);
-				test[i] += factor;
-				qout[i] += factor * qin[j];
-				
-				if(ic==test_i && jc==test_j){ printf("e %d %f %f %f %f %f %f\n",i,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000,factor);}
-				
-			} else if(zout[i+1] == zin[j+1] ){
-				
-				qout[i] += qin[j];
-				test[i] += 1.0;
-				if(ic==test_i && jc==test_j){ printf("f %d %f %f %f %f %f %f\n",i,zin[j],zin[j+1],zout[i],zout[i+1],qin[j]*1000,1.0);}
-			}
-		}
-
-	}}
-	if(ic==test_i && jc==test_j){
-		for(int i=0;i<zlevs;i++){ printf("%d %f\n",i,test[i]);}
-	}
-
-	return rainfall;
-
-}
-#endif
 
 /*********************************************************************
 * Substeps within RK3 loop
@@ -796,7 +507,6 @@ double get_CAPE(int i,int j,int k_p,double p_vapor,double p_temp){
 	}
 
 	return cape;
-	
 }
 
 /*********************************************************************
