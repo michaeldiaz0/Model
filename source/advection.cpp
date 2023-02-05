7e4efedae685b1fb228a3d4a45ea08ce8fee9ee8
Michael Diaz <michaeldiaz@Michaels-MacBook-Pro-3.local>
2023-02-04 16:15:06 -0800

    Remove commented-out code in boundaries. Minor change to documentation in advection and files.

--- source/advection.cpp
+++ source/advection.cpp
@@ -271,17 +271,17 @@ void advect_uv_velocity(double step,int il,int ih,int jl,int jh){
 	// This will become the flux on the western side of the leftmost interior (non-boundary) cell
 	compute_west_uv(il-1,jl,jh);
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes(i,jl-1,jh);
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 
@@ -306,25 +306,25 @@ void advect_uvw_velocity(double step,int il,int ih,int jl,int jh){
 	// This will become the flux on the western side of the leftmost interior (non-boundary) cell
 	compute_west(il-1,jl,jh);
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes(i,jl-1,jh);
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		LOOP2D_JK(jl,jh,1,NZ-1, UP(i,j,k) = UM(i,j,k) + step * u_tend_cell(i,j,k) )
 		LOOP2D_JK(jl,jh,1,NZ-1, VP(i,j,k) = VM(i,j,k) + step * v_tend_cell(i,j,k) )
 		LOOP2D_JK(jl,jh,2,NZ-1, WP(i,j,k) = WM(i,j,k) + step * w_tend_cell(i,j,k) )	
 
 	}
-	/********************************************************
-	* Calculate buoyancy term base on the microphysics option
-	*********************************************************/	
+	//-------------------------------------------------------
+	// Calculate buoyancy term base on the microphysics option
+	//-------------------------------------------------------	
 	if(MICROPHYSICS_OPTION == 0){
 		LOOP3D_IJK(il,ih,jl,jh,2,NZ-1,	WP(i,j,k) = WM(i,j,k) + step * dt*grav*0.5*( buoyancy_dry(i,j,k)+buoyancy_dry(i,j,k-1))	)
 	}
@@ -351,17 +351,17 @@ void advect_theta(double step,int il,int ih,int jl,int jh){
 	// This will become the flux on the western side of the leftmost interior cell
 	compute_west_scalar(il-1,jl,jh,&TH(0,0,0),&THBAR(0,0,0),&THCELL(0,0),&THBCELL(0,0));
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes_scalar(i,jl-1,jh,&TH(0,0,0),&THBAR(0,0,0),&THCELL(0,0),&THBCELL(0,0));
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 		
@@ -385,17 +385,17 @@ void advect_scalar(double step,int il,int ih,int jl,int jh,double *varm,double *
 	// This will become the flux on the western side of the leftmost interior cell
 	compute_west_scalar(il-1,jl,jh,var,scell);
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes_scalar(i,jl-1,jh,var,scell);
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 		
@@ -419,17 +419,17 @@ void advect_scalar(double step,int il,int ih,int jl,int jh,double *varm,double *
 	// This will become the flux on the western side of the leftmost interior cell
 	compute_west_scalar(il-1,jl,jh,var,varbase,scell,bcell);
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes_scalar(i,jl-1,jh,var,varbase,scell,bcell);
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 		
@@ -453,9 +453,9 @@ void advect_microphysics_cell(double step,int il,int ih,int jl,int jh){
 	// This will become the flux on the western side of the leftmost interior cell
 	compute_west_moisture(il-1,jl,jh);
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
@@ -465,9 +465,9 @@ void advect_microphysics_cell(double step,int il,int ih,int jl,int jh){
 		LOOP2D_JK(jl,jh,1,NZ-1, QCP(i,j,k) = QCM(i,j,k) + step * qc_tend_cell(i,j,k) )
 		LOOP2D_JK(jl,jh,1,NZ-1, QRP(i,j,k) = QRM(i,j,k) + step * qr_tend_cell(i,j,k) )
 #else
-		/********************************************************
+		//-------------------------------------------------------
 		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 		
@@ -492,28 +492,23 @@ void advect_microphysics_cell(double step,int il,int ih,int jl,int jh){
 void advect_ice_cell(double step,int il,int ih,int jl,int jh){
 
 	//-------------------------------------------------------------
-	// ADVECTION OF CLOUD ICE
-	//-------------------------------------------------------------
-	//advect_scalar(step,il,ih,jl,jh,qims,qis,qips,ice_cell);
-
-	//-------------------------------------------------------------
 	// ADVECTION OF SNOW
 	//-------------------------------------------------------------
 	// Calculate the zonal flux on the eastern side of innermost column of boundary points.
 	// This will become the flux on the western side of the leftmost interior cell
 	compute_west_scalar(il-1,jl,jh,qss,ice_cell);
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes_scalar_with_fallspeed(i,jl-1,jh,qss,sts,ice_cell);
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 		
@@ -529,17 +524,17 @@ void advect_ice_cell(double step,int il,int ih,int jl,int jh){
 	// This will become the flux on the western side of the leftmost interior cell
 	compute_west_scalar(il-1,jl,jh,qis,ice_cell);
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes_scalar_with_fallspeed(i,jl-1,jh,qis,its,ice_cell);
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 		
@@ -563,17 +558,17 @@ void advect_qv(double step,int il,int ih,int jl,int jh){
 	// This will become the flux on the western side of the leftmost interior cell
 	compute_west_scalar(il-1,jl,jh,&QV(0,0,0),&QBAR(0,0,0),&QVCELL(0,0),&QVBCELL(0,0));
 
-	/********************************************************
-	* For each point within the east-west range
-	*********************************************************/
+	//-------------------------------------------------------
+	// For each point within the east-west range
+	//-------------------------------------------------------
 	for(int i=il;i<ih;i++){
 
 		// compute all fluxes for a YZ cross section
 		compute_fluxes_scalar(i,jl-1,jh,&QV(0,0,0),&QBAR(0,0,0),&QVCELL(0,0),&QVBCELL(0,0));
 
-		/********************************************************
-		* For each point within the north-south range
-		*********************************************************/
+		//-------------------------------------------------------
+		// For each point within the north-south range
+		//-------------------------------------------------------
 		for(int j=jl;j<jh;j++){
 		for(int k=1;k<NZ-1;k++){
 		
